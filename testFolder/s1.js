308f46a02514f35d61935acba32ad439a7626c16
MaheuTaroo <58953303+MaheuTaroo@users.noreply.github.com>
2024-10-31 12:03:30 +0000

    02-00 answers

---
+++ testFolder/s1.js
@@ -0,0 +1,8 @@
+import { add } from './add.js'
+console.log(add(1,2))
+
+console.log("I'm s1")
+const mainHeading = document.getElementById("main-heading")
+console.log(mainHeading)
+const something = "hello"
+console.log(something)
